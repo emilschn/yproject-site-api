68f8cf8d7f11716b9dfbe5178a76fcee998ac744
Matthieu Moreau <matthieu.moreau@hetic.net>
2014-08-22 11:35:05 +0200

    [ADD] CORS Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+             new Nelmio\CorsBundle\NelmioCorsBundle(),
             new Liip\CacheControlBundle\LiipCacheControlBundle(),
             new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
             new Hautelook\TemplatedUriBundle\HautelookTemplatedUriBundle(),
