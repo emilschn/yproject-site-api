a4b5c3ed7ee8c3ac2af2f905cd22c9f4674c1208
Matthieu Moreau <matthieu.moreau@hetic.net>
2014-09-11 12:03:15 +0200

    [UPDATE] Default controller & template

--- src/WDG/CoreBundle/Controller/DefaultController.php
+++ src/WDG/CoreBundle/Controller/DefaultController.php
@@ -6,8 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('WDGCoreBundle:Default:index.html.twig', array('name' => 'World'));
+        return $this->render('WDGCoreBundle:Default:index.html.twig');
+    }
+
+    public function tokenAction()
+    {
+        return $this->render('WDGCoreBundle:Default:token.html.twig');
     }
 }
